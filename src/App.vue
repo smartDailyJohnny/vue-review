3b6e4b8e9c0319ce132bb3ea9a73335770c33632
Johnny <johnny.chang@smartdaily.com.tw>
2023-08-08 10:12:33 +0800

    changed setup

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,17 @@ import HomePage from './components/HomePage.vue'
   </main>
 </template>
 
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+</style>
