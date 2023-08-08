1f869aaf1cee65acf62a8a59707308e1660c0a10
Johnny <johnny.chang@smartdaily.com.tw>
2023-08-08 11:08:49 +0800

    moved imgs folder

--- src/components/build/RobotBuilder.vue
+++ src/components/build/RobotBuilder.vue
@@ -3,31 +3,31 @@
         <div>
             <div class="top-row">
                 <div class="top part">
-                    <img src="@/assets/images/head-big-eye.png" title="head" />
+                    <img src="@/data/images/head-big-eye.png" title="head" />
                     <button class="prev-selector">&#9668;</button>
                     <button class="next-selector">&#9658;</button>
                 </div>
             </div>
             <div class="middle-row">
                 <div class="left part">
-                    <img src="@/assets/images/arm-articulated-claw.png" title="left arm" />
+                    <img src="@/data/images/arm-articulated-claw.png" title="left arm" />
                     <button class="prev-selector">&#9650;</button>
                     <button class="next-selector">&#9660;</button>
                 </div>
                 <div class="center part">
-                    <img src="@/assets/images/torso-flexible-gauged.png" title="left arm" />
+                    <img src="@/data/images/torso-flexible-gauged.png" title="left arm" />
                     <button class="prev-selector">&#9668;</button>
                     <button class="next-selector">&#9658;</button>
                 </div>
                 <div class="right part">
-                    <img src="@/assets/images/arm-dual-claw.png" title="left arm" />
+                    <img src="@/data/images/arm-dual-claw.png" title="left arm" />
                     <button class="prev-selector">&#9650;</button>
                     <button class="next-selector">&#9660;</button>
                 </div>
             </div>
             <div class="bottom-row">
                 <div class="bottom part">
-                    <img src="@/assets/images/base-single-wheel.png" title="left arm" />
+                    <img src="@/data/images/base-single-wheel.png" title="left arm" />
                     <button class="prev-selector">&#9668;</button>
                     <button class="next-selector">&#9658;</button>
                 </div>
