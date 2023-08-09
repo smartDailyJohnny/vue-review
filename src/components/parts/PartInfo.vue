12e8c91cdbeda9db45496f7301f360d09d365da1
Johnny <johnny.chang@smartdaily.com.tw>
2023-08-09 11:07:47 +0800

    added notes to PartInfo

--- src/components/parts/PartInfo.vue
+++ src/components/parts/PartInfo.vue
@@ -4,18 +4,19 @@ import { useRoute } from 'vue-router';
 import parts from '@/data/parts';
 
 const route = useRoute()
-const part = computed(() => {
+const partObj = computed(() => {
     const partType = route.params.partType;
     const id = route.params.id;
-
-    return parts[partType].find(part => part.id === id);
+    // retrieve an array of parts based on the partType route param
+    // this function is responsible for finding the matching part
+    return parts[partType].find((item) => item.id === id);
 })
 </script>
 
 <template>
     <div id="PathInfo">
-        <h1>{{ part.title }}</h1>
-        <div>{{ part.description }}</div>
+        <h1>{{ partObj.title }}</h1>
+        <div>{{ partObj.description }}</div>
     </div>
 </template>
 
